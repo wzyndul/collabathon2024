d4df0392b3467a671423627b4a39c4375457f276
Wojciech Stobiński <tallandslim9@gmail.com>
2024-10-18 13:39:51 +0200

    Initial backend commit

---
+++ backend/src/main/java/com/stc/collabothon/CollabothonApplication.java
@@ -0,0 +1,13 @@
+package com.stc.collabothon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabothonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabothonApplication.class, args);
+	}
+
+}
